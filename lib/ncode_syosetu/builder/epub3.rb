ea6473e8986ca07cbd8957e6bd370e11f9f8e635
hogelog <konbu.komuro@gmail.com>
2014-09-22 21:40:24 +0900

    Fix invalid br tag

--- lib/ncode_syosetu/builder/epub3.rb
+++ lib/ncode_syosetu/builder/epub3.rb
@@ -15,7 +15,7 @@ module NcodeSyosetu
             date Time.now.to_s
 
             resources(workdir: tmpdir) do
-              toc_html = novel.toc.html.gsub(%r[<a href="/[^/]+/(\d+)/?">], '<a href="\1.html">')
+              toc_html = novel.toc.html.gsub(%r[<a href="/[^/]+/(\d+)/?">], '<a href="\1.html">').gsub(/<br>/, '<br />')
               File.write("toc.html", toc_html)
               nav "toc.html"
 
@@ -25,8 +25,9 @@ module NcodeSyosetu
                   if episode.is_a?(NcodeSyosetu::Model::Heading)
                     next_heading = episode.title
                   else
+                    episode_html = episode.html.gsub(/<br>/, '<br />')
                     html_path = "#{episode.number}.html"
-                    File.write(html_path, episode.html)
+                    File.write(html_path, episode_html)
                     file html_path
                     if next_heading
                       heading next_heading
