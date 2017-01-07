3572266c47f0c905d72614afb7d3a54871455fb7
hogelog <konbu.komuro@gmail.com>
2017-01-07 23:31:41 +0900

    Remove unused file

--- ncode-syosetu-ssml/lib/ncode/syosetu/ssml.rb
+++
@@ -1,9 +0,0 @@
-require "ncode/syosetu/ssml/version"
-
-module Ncode
-  module Syosetu
-    module Ssml
-      # Your code goes here...
-    end
-  end
-end
--- ncode-syosetu-ssml/lib/ncode/syosetu/ssml/version.rb
+++
@@ -1,7 +0,0 @@
-module Ncode
-  module Syosetu
-    module Ssml
-      VERSION = "0.1.0"
-    end
-  end
-end
