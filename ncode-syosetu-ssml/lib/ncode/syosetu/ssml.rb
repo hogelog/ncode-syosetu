1b9a894fcc843b4881fb7858ad8acfff5d6e4a00
hogelog <konbu.komuro@gmail.com>
2017-01-07 23:04:50 +0900

    Generate ncode-syosetu-ssml gem

---
+++ ncode-syosetu-ssml/lib/ncode/syosetu/ssml.rb
@@ -0,0 +1,9 @@
+require "ncode/syosetu/ssml/version"
+
+module Ncode
+  module Syosetu
+    module Ssml
+      # Your code goes here...
+    end
+  end
+end
---
+++ ncode-syosetu-ssml/lib/ncode/syosetu/ssml/version.rb
@@ -0,0 +1,7 @@
+module Ncode
+  module Syosetu
+    module Ssml
+      VERSION = "0.1.0"
+    end
+  end
+end
