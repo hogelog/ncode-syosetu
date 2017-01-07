c3b2e30c734063217f63b4326b835a00d0d73c3f
hogelog <konbu.komuro@gmail.com>
2017-01-07 14:52:30 +0900

    Generate ncode-syosetu-polly gem

---
+++ ncode-syosetu-polly/lib/ncode/syosetu/polly.rb
@@ -0,0 +1,9 @@
+require "ncode/syosetu/polly/version"
+
+module Ncode
+  module Syosetu
+    module Polly
+      # Your code goes here...
+    end
+  end
+end
---
+++ ncode-syosetu-polly/lib/ncode/syosetu/polly/version.rb
@@ -0,0 +1,7 @@
+module Ncode
+  module Syosetu
+    module Polly
+      VERSION = "0.1.0"
+    end
+  end
+end
